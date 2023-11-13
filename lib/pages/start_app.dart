8db5bd3d62a72315ef018d535eff2484483049e5
bintangsholu21 <bintangsholu@gmail.com>
2023-11-13 14:02:29 +0700

    Inisiasi dan create start_app

---
+++ lib/pages/start_app.dart
@@ -0,0 +1,120 @@
+// lib/pages/start_app.dart
+import 'package:flutter/material.dart';
+import 'register_page.dart';
+import 'login_page.dart';
+
+class StartApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background Image
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('../assets/background.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // Title and Subtitle Container
+          Positioned(
+            top: 80.0,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Title
+                  Text(
+                    'GoConcert',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  // Subtitle
+                  Text(
+                    'Dengarkan Musik Live Favoritmu\nPesan Tiketmu Hari Ini!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 32),
+                ],
+              ),
+            ),
+          ),
+          // Buttons Container
+          Positioned(
+            bottom: 50.0,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Pesan Sekarang Button
+                  Container(
+                    width: 300.0,
+                    height: 50.0,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RegisterPage()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFF5E8C7),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                      ),
+                      child: Text(
+                        'Pesan Sekarang!',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  // Sudah memiliki akun Text
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                    },
+                    child: Text(
+                      'Sudah memiliki akun? Masuk',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontFamily: 'Poppins',
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
